ca0f60eeb3be8345e3ce2576b0ed12bfc63edc8e
solspyre <solnurkarim@gmail.com>
2018-02-04 17:37:19 +0600

    signup page added. login page and styles fixed

--- js/authentication.js
+++ js/authentication.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+	
+			// --- checkbox toggle ---
+
 			$('#check').click(function(){
 				$('.fa-square').css('visibility', function(){
 					return ($('.fa-square').css('visibility') == 'visible') ? 'hidden' : 'visible';
@@ -12,4 +15,22 @@ $(document).ready(function(){
 					$('#checkbox').prop('checked', false)
 				}
 			});
+
+
+			// --- bootstrap form validation ---
+
+			window.addEventListener('load', function() {
+			    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+			    var forms = document.getElementsByClassName('needs-validation');
+			    // Loop over them and prevent submission
+			    var validation = Array.prototype.filter.call(forms, function(form) {
+			      form.addEventListener('submit', function(event) {
+			        if (form.checkValidity() === false) {
+			          event.preventDefault();
+			          event.stopPropagation();
+			        }
+			        form.classList.add('was-validated');
+			      }, false);
+			    });
+			}, false);
 })
